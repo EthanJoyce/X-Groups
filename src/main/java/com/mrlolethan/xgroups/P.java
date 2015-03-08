c13dd2d2a5d0a49265517c5be87782203cea0e5f
mrlolethan <me@mrlolethan.com>
2015-03-08 12:54:38 -0230

    Initial commit.

---
+++ src/main/java/com/mrlolethan/xgroups/P.java
@@ -0,0 +1,19 @@
+package com.mrlolethan.xgroups;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class P extends JavaPlugin {
+
+	@Override
+	public void onLoad() {
+	}
+
+	@Override
+	public void onEnable() {
+	}
+
+	@Override
+	public void onDisable() {
+	}
+
+}
